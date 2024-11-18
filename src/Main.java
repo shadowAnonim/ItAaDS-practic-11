db88a5d5b3a63b2acd6d4fb22b88e525e0a63c11
Аноним Евгений <shadow.anonim@yandex.ru>
2024-11-18 15:17:06 +0300

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -11,21 +11,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         Calendar cal = Calendar.getInstance();
+        cal.setLenient(false);
+
         System.out.println("Введите дату: Год Месяц Число");
         cal.set(Calendar.YEAR, sc.nextInt());
         cal.set(Calendar.MONTH, sc.nextInt());
         cal.set(Calendar.DAY_OF_MONTH, sc.nextInt());
         sc.nextLine();
         System.out.println("Введите время: Часы Минуты");
-        cal.set(Calendar.HOUR, sc.nextInt());
+        cal.set(Calendar.HOUR_OF_DAY, sc.nextInt());
         cal.set(Calendar.MINUTE, sc.nextInt());
 
-        /*System.out.println("Введённая дата " +
-                new SimpleDateFormat("yyyy-MM-dd hh:mm").format(cal.getTime()));*/
+        Calendar cur = Calendar.getInstance();
+        System.out.println("Введённая дата " +
+                new SimpleDateFormat("yyyy-MM-dd hh:mm").format(cal.getTime()));
 
-        System.out.println(cal.get(Calendar.MONTH));
-        System.out.println(cal.get(Calendar.DAY_OF_MONTH));
-
-
+        if (cal.compareTo(cur) > 0)
+            System.out.println("Больше текущего времени");
+        else if (cal.compareTo(cur) < 0)
+            System.out.println("Меньше текущего времени");
+        else
+            System.out.println("Равно текущему времени");
     }
 }
